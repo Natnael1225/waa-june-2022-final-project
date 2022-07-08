b6f579da504f8040d0462f1d00dcffd038883de3
keletazghi <tesfazghikulut@gmail.com>
2022-07-08 01:40:36 -0500

    added

--- Almuni/src/main/java/com/example/almuni/service/IFacultyService.java
+++ Almuni/src/main/java/com/example/almuni/service/IFacultyService.java
@@ -1,10 +1,5 @@
 package com.example.almuni.service;
 
-import com.example.almuni.Dto.FacultyDto;
-import com.example.almuni.entity.JobAdvertisement;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.http.ResponseEntity;
-
 import java.util.List;
 
 public interface IFacultyService {
